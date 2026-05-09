91fd4ad54b48469ec13230fc7ea051c9a711e28c
Your Name <babulhossan.info@gmail.com>
2026-05-09 21:17:39 +0600

    complete navbar banner and footer section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+
+        <div className=' shadow-md '>
+
+            <div className='w-11/12 mx-auto flex justify-between items-center py-5'>
+            <ul className='flex gap-5'>
+                <li>
+                    <Link href='/'>Home</Link>
+                </li>
+                <li>
+                    <Link href='/destinations'>Destinations</Link>
+                </li>
+                <li>
+                    <Link href='/myBookings'>My Bookings</Link>
+                </li>
+                <li>
+                    <Link href='/admin'>Admin</Link>
+                </li>
+            </ul>
+            <div>
+                <Image 
+                src={'/assets/Wanderlast.png'}
+                width={150}
+                height={150}
+                alt='logo'
+                ></Image>
+            </div>
+
+            <ul className='flex gap-5'>
+                <li>
+                    <Link href='/profile'>Profile</Link>
+                </li>
+                <li>
+                    <Link href='/login'>Login</Link>
+                </li>
+                <li>
+                    <Link href='/signUp'>Sign Up</Link>
+                </li>
+                
+            </ul>
+
+        </div>
+        </div>
+    );
+};
+
+export default Navbar;
